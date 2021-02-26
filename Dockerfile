ad03c7426f9b70ceb02a916f108cae4e6e85f561
Sylvain Ducamp <sylducam@z4r4p7.42lyon.fr>
2021-02-26 17:20:59 +0100

    keep on going with the tutorials, starting with nginx

--- Dockerfile
+++ Dockerfile
@@ -6,13 +6,13 @@
 #    By: sylducam <marvin@42.fr>                    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2021/02/25 11:33:57 by sylducam          #+#    #+#              #
-#    Updated: 2021/02/25 11:47:35 by sylducam         ###   ########lyon.fr    #
+#    Updated: 2021/02/26 16:57:58 by sylducam         ###   ########lyon.fr    #
 #                                                                              #
 # **************************************************************************** #
 
 FROM debian:buster
 
-RUN apt-get update && apt-get install -y sudo openssl nginx && sudo apt install -y mariadb-server php-fpm php-mysql
+RUN apt-get update && apt-get install -y sudo openssl nginx && sudo apt install -y wget mariadb-server mariadb-client php-fpm php-mysql
 
 COPY srcs/ .
 
--- srcs/script.sh
+++ srcs/script.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
+mkdir var/www/localhost
+
 #SERVICE LAUNCH
 service php7.3-fpm start
 service nginx start
 
 #SSL CERTIFICATE
-sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/nginx-selfsigned.key -out /etc/ssl/certs/nginx-selfsigned.crt -subj "/C=FR/ST=AURA/L=Lyon/O=42/CN=127.0.0.1"
-sudo openssl dhparam -out /etc/nginx/dhparam.pem 2048
+sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/nginx-selfsigned.key -out /etc/ssl/certs/nginx-selfsigned.crt -subj "/C=FR/ST=AURA/L=Lyon/O=42/OU=sylducam/CN=localhost"
 sudo mv self-signed.conf /etc/nginx/snippets/
 sudo mv ssl-params.conf /etc/nginx/snippets/
 sudo cp /nginx_params/default /etc/nginx/sites-available/default
@@ -15,3 +16,8 @@ sudo cp /nginx_params/default /etc/nginx/sites-available/default
 service mysql start
 sudo mysql_secure_installation
 mysql -u root  < mysql.sql
+
+#WORDPRESS
+wget https://wordpress.org/latest.tar.gz
+tar xf latest.tar.gz
+mv wordpress var/www/localhost/
