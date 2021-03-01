5bc17e9ce678715033d688e4c97dc21b7ff0e7b9
Sylvain Ducamp <sylducam@z4r1p1.42lyon.fr>
2021-03-01 17:30:25 +0100

    it seems that nginx starts with my localhost file the issue was that the dhparam command was needed, as dhparam is metionned in the ssl-params.conf file. Try and delete the line in ssl-params.conf and see if you still need the dhparam command

--- srcs/start.sh
+++ srcs/start.sh
@@ -1,28 +1,28 @@
 #!/bin/bash
 
 #NGINX
-mkdir -p var/www/localhost/html
+mkdir -p /var/www/localhost/html
 sudo chown -R $USER:$USER /var/www/localhost
 sudo chmod -R 755 /var/www/localhost
-mv default etc/nginx/sites-available
-sudo ln -s /etc/nginx/sites-available/default /etc/nginx/sites-enabled/
+mv localhost /etc/nginx/sites-available
+sudo ln -s /etc/nginx/sites-available/localhost /etc/nginx/sites-enabled/
 
 #SERVICE LAUNCH
 service php7.3-fpm start
 service nginx start
 
 #SSL CERTIFICATE
-sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/nginx-selfsigned.key -out /etc/ssl/certs/nginx-selfsigned.crt -subj "/C=FR/ST=AURA/L=Lyon/O=42/OU=sylducam/CN=localhost"
-sudo mv self-signed.conf /etc/nginx/snippets/
-sudo mv ssl-params.conf /etc/nginx/snippets/
-sudo cp /nginx_params/default /etc/nginx/sites-available/default
+sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout etc/ssl/private/nginx-selfsigned.key -out etc/ssl/certs/nginx-selfsigned.crt -subj "/C=FR/ST=AURA/L=Lyon/O=42/OU=sylducam/CN=localhost/emailAddress=sylducam@student.42-lyon.fr"
+# essayes de ne pas mettre cette ligne et de virer sa correspondance dans ssl-params.conf sudo openssl dhparam -out /etc/nginx/dhparam.pem 2048
+sudo mv self-signed.conf /etc/nginx/snippets
+sudo mv ssl-params.conf /etc/nginx/snippets
 
 #MYSQL
 service mysql start
 sudo mysql_secure_installation
-mysql -u root  < mysql.sql
+mysql -u root < mysql.sql
 
 #WORDPRESS
 wget https://wordpress.org/latest.tar.gz
 tar xf latest.tar.gz
-mv wordpress var/www/localhost/
+mv wordpress /var/www/localhost
