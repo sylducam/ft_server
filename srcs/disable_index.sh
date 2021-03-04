747754a102c7f5a4d88f5a52472df7e2fadfa97a
Sylvain Ducamp <sylducam@z4r5p7.42lyon.fr>
2021-03-04 15:46:47 +0100

    all is clean and finished

--- srcs/disable_index.sh
+++ srcs/disable_index.sh
@@ -1,9 +1,9 @@
 #!bin/bash
 
-if cmp -s /nginx_params/default /etc/nginx/sites-available/default
+if cmp -s default /etc/nginx/sites-available/default
 then
-	cp /nginx_params/indexoff /etc/nginx/sites-available/default
+	cp autoindexoff /etc/nginx/sites-available/default
 else
-	cp /nginx_params/default /etc/nginx/sites-available/default
+	cp default /etc/nginx/sites-available/default
 fi
 service nginx restart
--- srcs/start.sh
+++ srcs/start.sh
@@ -4,38 +4,16 @@
 service php7.3-fpm start
 service nginx start
 
-#NGINX
-#mkdir -p /var/www/localhost/html
-#sudo chown -R $USER:$USER /var/www/localhost
-#sudo chmod -R 755 /var/www/localhost
-#mv localhost /etc/nginx/sites-available
-#mv default /etc/nginx/sites-available
-#sudo ln -s /etc/nginx/sites-available/localhost /etc/nginx/sites-enabled/
-#sudo ln -s /etc/nginx/sites-available/default /etc/nginx/sites-enabled/
-
 #SSL CERTIFICATE
 sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/nginx-selfsigned.key -out /etc/ssl/certs/nginx-selfsigned.crt -subj "/C=FR/ST=AURA/L=Lyon/O=42/OU=sylducam/CN=localhost/emailAddress=sylducam@student.42-lyon.fr"
-#essayes de ne pas mettre cette ligne et de virer sa correspondance dans ssl-params.conf
-#sudo openssl dhparam -out /etc/nginx/dhparam.pem 2048
 sudo mv self-signed.conf /etc/nginx/snippets
 sudo mv ssl-params.conf /etc/nginx/snippets
 sudo cp default /etc/nginx/sites-available/default
-#SSL_CERTS=/etc/ssl/certs/localhost.pem
-#SSL_CERTS_KEY=/etc/ssl/certs/localhost-key.pem
-#if [[ ! -f "$SSL_CERTS" ]] || [[ ! -f "$SSL_CERTS_KEY" ]]
-#then
-#	echo "SSL certs does not exists boyzzz"
-#	chmod +x mkcert
-#	./mkcert -install
-#	./mkcert localhost
-#	mv localhost.pem /etc/ssl/certs/
-#	mv localhost-key.pem /etc/ssl/certs/
-#fi
 
 #MYSQL
 service mysql start
 sudo mysql_secure_installation
-mysql -u root  < mysql.sql # ca ne marche pas
+mysql -u root  < mysql.sql
 
 #WORDPRESS
 wget https://wordpress.org/latest.tar.gz
