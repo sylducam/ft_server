58fbf59c5998b88691ed9e491564a84a97aa8c0d
Sylvain Ducamp <sylducam@z4r2p1.42lyon.fr>
2021-03-05 11:28:14 +0100

    now everything is working, wordpress and phpmyadmin run and display. I could modify a comment on wordpress via phpmyadmin and also the mysql_secure_installation autocompletes

--- README.md
+++ README.md
@@ -2,7 +2,12 @@
 
 Project meant to discover and use Docker by setting a unique container running with Debian Buster and setting an Nginx web server. This web server runs a Wordpress, PhpMyAdmin and MySQL.
 
-# commands to run the project
+# Commands to run the project
 
 docker build -t ft_server .
 docker run -p 80:80 -p 443:443 -it --rm --name ft_server_ct ft_server
+
+to keep it running and open a terminal in Debian :
+
+docker run -t -d -p 80:80 -p 443:443 --name ft_server_ct ft_server
+docker exec -it ft_server_ct bash
--- srcs/start.sh
+++ srcs/start.sh
@@ -3,6 +3,9 @@
 #SERVICE LAUNCH
 service php7.3-fpm start
 service nginx start
+chown -R www-data /var/www/*
+chmod -R 755 /var/www/*
+ln -s etc/nginx/sites-available/default etc/nginx/sites-enabled
 
 #SSL CERTIFICATE
 sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/nginx-selfsigned.key -out /etc/ssl/certs/nginx-selfsigned.crt -subj "/C=FR/ST=AURA/L=Lyon/O=42/OU=sylducam/CN=localhost/emailAddress=sylducam@student.42-lyon.fr"
@@ -12,7 +15,28 @@ sudo cp default /etc/nginx/sites-available/default
 
 #MYSQL
 service mysql start
-sudo mysql_secure_installation
+aptitude -y install expect
+SECURE_MYSQL=$(expect -c "
+set timeout 10
+spawn mysql_secure_installation
+expect \"Enter current password for root (enter for none):\"
+send \"$MYSQL\r\"
+expect \"Change the root password?\"
+send \"n\r\"
+expect \"Remove anonymous users?\"
+send \"n\r\"
+expect \"Disallow root login remotely?\"
+send \"n\r\"
+expect \"Remove test database and access to it?\"
+send \"n\r\"
+expect \"Reload privilege tables now?\"
+send \"n\r\"
+expect eof
+")
+
+echo "$SECURE_MYSQL"
+
+aptitude -y purge expect
 mysql -u root  < mysql.sql
 
 #WORDPRESS
